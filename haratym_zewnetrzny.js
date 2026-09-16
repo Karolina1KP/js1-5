c0ad1e759a3b7375931d8676a4874df2ef31ba0d
Karolina <karolinka.haratym1@gmail.com>
2026-09-16 22:08:59 +0200

    Add files via upload

---
+++ haratym_zewnetrzny.js
@@ -0,0 +1,5 @@
+document.write("Mój numer w dzienniku to 4".fontcolor("#000000")+"<br>");
+document.write("Pierwszy wiersz to: 13 &nbsp;Tak &nbsp;Nie&nbsp; 6".fontcolor("#000000")+"<br>");
+document.write("Drugi wiersz to: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2 &nbsp;&nbsp;Nie &nbsp;Tak &nbsp;6".fontcolor("#000000")+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("navy").bold().fontsize(6)+"<br>"); 
+document.write((document.lastModified).strike().fontcolor("silver"));
